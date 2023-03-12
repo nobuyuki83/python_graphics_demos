344acbe167ee81f93143567abd5f0d65e42483ce
Nobuyuki <n.umetani@gmail.com>
2023-03-12 18:03:31 +0900

    added a demo 09_tri_select.py

---
+++ 09_tri_select.py
@@ -0,0 +1,72 @@
+import pathlib
+import moderngl
+from PyQt5 import QtWidgets, QtCore
+import numpy
+
+from util_moderngl_qt.drawer_meshpos import DrawerMesPos, ElementInfo
+from util_moderngl_qt.drawer_meshunindex import DrawerMeshUnindex
+import util_moderngl_qt.qtglwidget_viewer3
+import del_msh
+import del_srch
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        path_file = pathlib.Path('.') / 'asset' / 'bunny_1k.obj'
+        self.tri2vtx, vtx2xyz = del_msh.del_msh.load_wavefront_obj_as_triangle_mesh(str(path_file))
+        self.vtx2xyz = del_msh.centerize_scale_3d_points(vtx2xyz)
+
+        edge2vtx = del_msh.edges_of_uniform_mesh(self.tri2vtx, self.vtx2xyz.shape[0])
+        drawer_edge = DrawerMesPos(
+            vtx2xyz=self.vtx2xyz.astype(numpy.float32),
+            list_elem2vtx=[
+                ElementInfo(index=edge2vtx, color=(0, 0, 0), mode=moderngl.LINES)]
+        )
+
+        tri2vtx2xyz = del_msh.unidex_vertex_attribute_for_triangle_mesh(self.tri2vtx, self.vtx2xyz)
+        self.tri2flag = numpy.zeros(self.tri2vtx.shape[0], dtype=numpy.int32)
+
+        drawer_face = DrawerMeshUnindex(
+            tri2vtx2xyz=tri2vtx2xyz.astype(numpy.float32),
+        )
+
+        super().__init__()
+        self.resize(640, 480)
+        self.setWindowTitle('Mesh Viewer')
+        self.glwidget = util_moderngl_qt.qtglwidget_viewer3.QtGLWidget_Viewer3(
+            [drawer_face, drawer_edge])
+        self.glwidget.mousePressCallBack.append(self.mouse_press_callback)
+        self.setCentralWidget(self.glwidget)
+
+    def mouse_press_callback(self, event):
+        if event.modifiers() & QtCore.Qt.KeyboardModifier.ShiftModifier:
+            return
+        if event.modifiers() & QtCore.Qt.KeyboardModifier.AltModifier:
+            return
+        src, direction = self.glwidget.nav.picking_ray()
+        pos, tri_index = del_srch.first_intersection_ray_meshtri3(
+            numpy.array(src.xyz).astype(numpy.float32), numpy.array(direction.xyz).astype(numpy.float32),
+            self.vtx2xyz, self.tri2vtx)
+        num_tri = self.tri2vtx.shape[0]
+        tri2node2color = numpy.zeros([num_tri, 3, 3], dtype=numpy.float32)
+        self.tri2flag[tri_index] = 1 - self.tri2flag[tri_index]
+        tri2node2color[self.tri2flag == 0, :, 0] = 1.
+        tri2node2color[self.tri2flag == 0, :, 1] = 1.
+        tri2node2color[self.tri2flag == 0, :, 2] = 1.
+        tri2node2color[self.tri2flag == 1, :, 0] = 1
+        tri2node2color[self.tri2flag == 1, :, 1] = 0
+        tri2node2color[self.tri2flag == 1, :, 2] = 0
+        self.glwidget.list_drawer[0].update_color(tri2node2color)
+        self.glwidget.update()
+
+
+def main():
+    with QtWidgets.QApplication([]) as app:
+        win = MainWindow()
+        win.show()
+        app.exec()
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -12,11 +12,23 @@
 
 <img src="https://raw.githubusercontent.com/nobuyuki83/demos_py_qt_mdngl/doc/06_thumbnail.png" width=320>
 
+## 07_blendshape_single.py
+
+direct manipulation of blendshape with single control point
+
 <img src="https://raw.githubusercontent.com/nobuyuki83/demos_py_qt_mdngl/doc/07_thumbnail.png" width=320>
+
+## 08_blendshape_multi.py
+
+direct manipulation of blendshape with multiple control points
 
 <img src="https://raw.githubusercontent.com/nobuyuki83/demos_py_qt_mdngl/doc/08_thumbnail.png" width=320>
 
+## [09_tri_select.py](09_tri_select.py)
 
+user interface for selecting/unselecting triangles  
+
+<img src="https://raw.githubusercontent.com/nobuyuki83/demos_py_qt_mdngl/doc/09_thumbnail.png" width=320>
 
 
 
